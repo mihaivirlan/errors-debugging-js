17a9b364df178f16d066065594ee21ade8d0f5d2
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-11 22:43:12 +0300

    Handling Errors with try and catch

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-var a = 2;
-var b = 5;
-var c;
-var d = 1000;
-for (var i = 0; i < 5; i++) {
-    if (i > a && i < b) {
-        c = 1000;
-    }
-    if (i === 3) {
-        d *= 2;
-    }
+    //Handling Errors with try and catch
+try {
+    test();
+} catch (error) {
+    console.log(errpr);
+    
+} finally {
+    console.log('Finally!');
 }
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
 
 
